917529937788e56a80b37fc7296a05f4c427c952
Harekasa <2459743667@qq.com>
2026-05-08 00:59:51 +0800

    chore:添加.gitignore

--- build/behaviortree_cpp/examples/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harekasa/harekasa_sentry_ws/src/behaviortree_cpp/examples
-# Build directory: /home/harekasa/harekasa_sentry_ws/build/behaviortree_cpp/examples
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/behaviortree_cpp/sample_nodes/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harekasa/harekasa_sentry_ws/src/behaviortree_cpp/sample_nodes
-# Build directory: /home/harekasa/harekasa_sentry_ws/build/behaviortree_cpp/sample_nodes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
