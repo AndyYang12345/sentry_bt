e2a602650e77b49f02580c351d38dd331baa2b91
Harekasa <2459743667@qq.com>
2026-05-11 20:05:53 +0800

    feat: 完成弹药判断节点

--- src/decision_process/include/decision_process/treenode/conditions/combat_conditions.hpp
+++ src/decision_process/include/decision_process/treenode/conditions/combat_conditions.hpp
@@ -1,3 +1,25 @@
 #pragma once
 #include <behaviortree_cpp/bt_factory.h>
 #include "decision_process/include/interfaces/interfaces.hpp"
+
+using namespace BT;
+
+class CheckAmmo: public ConditionNode{
+public:
+    CheckAmmo(const std::string& name, const NodeConfig& config)
+        : ConditionNode(name, config){}
+    static PortsList providedPorts(){
+        return {
+            InputPort<uint16_t>("ammo", "当前弹药数量")
+        };
+    }
+
+    NodeStatus tick() override{
+        uint16_t ammo;
+        if (ammo > 0){
+            return NodeStatus::SUCCESS;
+        } else {
+            return NodeStatus::FAILURE;
+        }
+    }
+};
