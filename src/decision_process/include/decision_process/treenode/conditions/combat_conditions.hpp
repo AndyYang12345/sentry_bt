ed5ba08ca8befa4324864cb8281bce600ef63004
Harekasa <2459743667@qq.com>
2026-05-11 20:21:31 +0800

    feat: 完成敌人可见度判断节点

--- src/decision_process/include/decision_process/treenode/conditions/combat_conditions.hpp
+++ src/decision_process/include/decision_process/treenode/conditions/combat_conditions.hpp
@@ -23,3 +23,36 @@ public:
         }
     }
 };
+
+class CheckEnemyVisible: public ConditionNode{
+public:
+    CheckEnemyVisible(const std::string& name, const NodeConfig& config)
+        : ConditionNode(name, config){}
+    static PortsList providedPorts(){
+        return {
+            InputPort<std::vector<bool>>("enemy_visible", "敌人是否可见"),
+            OutputPort<bool>("blur_target", "是否存在模糊目标")
+        };
+    }
+
+    NodeStatus tick() override{
+        std::vector<bool> enemy_visible;
+        if (getInput("enemy_visible", enemy_visible)){
+            if(enemy_visible.empty()){
+                setOutput("blur_target", false);
+                return NodeStatus::FAILURE;
+            }
+            if(enemy_visible[0]){
+                setOutput("blur_target", true);
+                return NodeStatus::SUCCESS;
+            }else{
+                setOutput("blur_target", false);
+            }
+            for (size_t i = 1; i < enemy_visible.size(); ++i){
+                bool visible = enemy_visible[i];
+                return visible ? NodeStatus::SUCCESS : NodeStatus::FAILURE;
+            }
+        }
+        return NodeStatus::FAILURE;
+    }
+};
