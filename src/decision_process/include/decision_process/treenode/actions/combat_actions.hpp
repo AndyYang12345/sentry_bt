c5bda10770f8f36d5222fa4b266fa34dce55779b
Harekasa <2459743667@qq.com>
2026-05-11 19:50:35 +0800

    feat: 完成开火动作节点

--- src/decision_process/include/decision_process/treenode/actions/combat_actions.hpp
+++ src/decision_process/include/decision_process/treenode/actions/combat_actions.hpp
@@ -1,3 +1,27 @@
 #pragma once
 #include <behaviortree_cpp/bt_factory.h>
+#include <string>
 #include "decision_process/include/interfaces/interfaces.hpp"
+
+/*
+    @brief 战斗，爽！
+    @param 无
+    @return 成功：直接开火！！
+*/
+using namespace BT;
+
+class AttackEnemy: public SyncActionNode{
+public:
+    AttackEnemy(const std::string& name, const NodeConfig& config)
+        : SyncActionNode(name, config){}
+    static PortsList providedPorts(){
+        return {
+            OutputPort<uint8_t>("shoot_mode", "设置开火状态")
+        };
+    }
+
+    NodeStatus tick() override{
+        setOutput("shoot_mode", 1);
+        return NodeStatus::SUCCESS;
+    }
+};
